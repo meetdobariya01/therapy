a8e51ab4b6020e63ce2988073a021906805be3d2
meetdobariya01 <meetdobariya87@gmail.com>
2025-11-24 16:40:35 +0530

    Update files

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import Header from "../../components/header/header";
+import Footer from "../../components/footer/footer";
 
 const About = () => {
   return (
     <div>
-      
+      {/* Header can be added here if needed */}
+      <Header />
+
+      {/* Footer can be added here if needed */}
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default About;
